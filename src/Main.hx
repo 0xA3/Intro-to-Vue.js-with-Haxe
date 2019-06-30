a4b7bdbcddbc0721c4a2d4ff69347b83675cb13a
0xA3 <deslac@protonmail.ch>
2019-06-30 17:18:32 +0200

    added computed properties

--- src/Main.hx
+++ src/Main.hx
@@ -8,26 +8,33 @@ class Main {
 			el: '#app',
 			data: {
 				product: "Socks",
-				image: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
-				inStock: true,
+				brand: "Vue Masterful",
+				selectedVariant: 0,
 				details: ["80% cotton", "20% polyester", "Gener-neutral"],
 				variants: [
 					{
 						variantId: 2234,
 						variantColor: "green",
-						variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+						variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+						variantQuantity: 10
 					},
 					{
 						variantId: 2235,
 						variantColor: "blue",
-						variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+						variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+						variantQuantity: 0
 					}
 				],
 				cart: 0
 			},
 			methods: {
 				addToCart: () -> Lib.nativeThis.cart += 1,
-				updateProduct: ( variantImage ) -> Lib.nativeThis.image = variantImage
+				updateProduct: ( index ) -> Lib.nativeThis.selectedVariant = index
+			},
+			computed: {
+				title: () -> '${Lib.nativeThis.brand} ${Lib.nativeThis.product}',
+				image: () -> Lib.nativeThis.variants[Lib.nativeThis.selectedVariant].variantImage,
+				inStock: () -> Lib.nativeThis.variants[Lib.nativeThis.selectedVariant].variantQuantity
 			}
 		});
 	}
