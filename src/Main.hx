39e7f5e6af00a3add94329fadfd66bc530e493e2
0xA3 <deslac@protonmail.ch>
2019-07-27 11:35:18 +0200

    added display of reviews

--- src/Main.hx
+++ src/Main.hx
@@ -39,6 +39,18 @@ class Main {
 						>Add to Cart
 					</button>
 
+					<div>
+						<h2>Reviews</h2>
+						<p v-if="!reviews.length">There are no reviews yet.</p>
+						<ul>
+							<li v-for="review in reviews">
+								<p>{{ review.name }}</p>
+								<p>Rating: {{ review.rating }}</p>
+								<p>{{ review.review }}</p>
+							</li>
+						</ul>
+					</div>
+
 					<product-review @review-submitted="addReview"></product-review>
 				</div>
 			',
